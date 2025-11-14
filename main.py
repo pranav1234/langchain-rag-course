emarco177/ice_breaker

Add RAG query system with retrieval chain
- Implement vector store retrieval using Pinecone
- Create retrieval chain with LangChain hub prompt
- Set up ChatOpenAI for response generation
- Add example query about Pinecone in machine learning
- Combine document retrieval with LLM generation

🤖 Generated with [Claude Code](https://claude.ai/code)

Co-Authored-By: Claude <noreply@anthropic.com>

main.py
@@ -1,0 +1,37 @@
+import os
+
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    print(" Retrieving...")
+
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI()
+
+    query = "what is Pinecone in machine learning?"
+    chain = PromptTemplate.from_template(template=query) | llm
+    # result = chain.invoke(input={})
+    # print(result.content)
+
+    vectorstore = PineconeVectorStore(
+        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    retrival_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrival_chain.invoke(input={"input": query})
+
+    print(result)

pranav1234/langchain-rag-course

main.py
@@ -1,0 +1,37 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_classic import hub
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+
+if __name__ == "__main__":
+    print(" Retrieving...")
+
+    embeddings = OpenAIEmbeddings()
+    llm = ChatOpenAI()
+
+    query = "what is Pinecone in machine learning?"
+    chain = PromptTemplate.from_template(template=query) | llm
+    # result = chain.invoke(input={})
+    # print(result.content)
+
+    vectorstore = PineconeVectorStore(
+        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(llm, retrieval_qa_chat_prompt)
+    retrival_chain = create_retrieval_chain(
+        retriever=vectorstore.as_retriever(), combine_docs_chain=combine_docs_chain
+    )
+
+    result = retrival_chain.invoke(input={"input": query})
+
+    print(result)